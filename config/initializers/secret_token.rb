a40251b81455c2a39a186330efe51730410bb8ce
Javen Chen <jchen@voxeo.com>
2012-10-21 11:00:03 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgilecsNet::Application.config.secret_token = '150739da47939d008cfaf741481945c63404f3a5d7dbb9178aa28117ba917cf1987ccfadb2a536dda05c74cd0e8b26adff430b5d7e95b5e06bc58f5a0cc31846'
